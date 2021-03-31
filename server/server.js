a00a4baf554803cf85c2e61a5e85a7d22cc85b12
Andrew Jones <aej11@hotmail.com>
2021-03-31 12:38:41 -0400

    Added Firebase connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
+require('dotenv').config()
+const firebase = require('firebase/app')
+require('firebase/firestore')
 var express = require('express')
 var app = express()
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyDTcOhMHYO4q9iqKBD75sicIxiBLJnFS2s',
+    authDomain: 'meal-sharing-4e92e.firebaseapp.com',
+    projectId: 'meal-sharing-4e92e',
+    storageBucket: 'meal-sharing-4e92e.appspot.com',
+    messagingSenderId: '760425941932',
+    appId: '1:760425941932:web:52245590ebd6b7b85ac809',
+})
+
+require('./db_sample')
 
 app.get('/sample', require('./api/sample'))
 
