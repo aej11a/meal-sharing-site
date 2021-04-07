6741d3d66bfb37e36cd3135aca8c26adcf3e113e
JD Pablo <jdpablo1002@gmail.com>
2021-04-07 12:35:17 -0400

    scrapped cloud functions, turned geocoding into function

---
+++ client/src/forward-geocoding.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+
+export async function getCoordinates(address) {
+    try {
+        let apiCall = await getApiCall(address)
+        return {
+            latitude: apiCall.data.data[0].latitude,
+            longitude: apiCall.data.data[0].longitude,
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function getApiCall(address) {
+    const params = {
+        access_key: process.env.GEOCODE_KEY,
+        query: address,
+    }
+    return axios.get('http://api.positionstack.com/v1/forward', { params })
+}
