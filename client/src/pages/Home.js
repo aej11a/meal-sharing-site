2b1d281f72117235f3b54ee4cf681e7488728763
Andrew Jones <aej11@hotmail.com>
2021-04-29 13:53:15 -0400

    restyled meal list items

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import { db } from '../firebase'
+import { MealListItem } from '../components/MealListItem'
 import Button from '@material-ui/core/Button'
+import Chip from '@material-ui/core/Chip'
 import MTextField from '@material-ui/core/TextField'
 const TextField = (props) => (
     <MTextField
@@ -10,52 +12,49 @@ const TextField = (props) => (
 )
 const getMeals = async (allergens) => {
     const snapshot = await db.collection('meals').get()
-    console.log()
+    const rawMealsData = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+    }))
     if (allergens && allergens.length) {
-        return snapshot.docs
-            .map((doc) => doc.data())
-            .filter((meal) => {
-                const hasDishes = !!meal.dishes && meal.dishes.length > 0
-                if (!allergens) return true
-                else {
-                    const isEachAllergenInDish = allergens.map((allergen) => {
-                        const dishIncludesAllergen = (dish) =>
-                            dish.dishIngredients &&
-                            dish.dishIngredients.includes(allergen)
-                        return (
-                            hasDishes &&
-                            typeof meal.dishes.find(dishIncludesAllergen) ===
-                                'undefined'
-                        )
-                    })
-                    return isEachAllergenInDish.includes(true)
-                }
-            })
+        return rawMealsData.filter((meal) => {
+            const hasDishes = !!meal.dishes && meal.dishes.length > 0
+            if (!allergens) return true
+            else {
+                const isEachAllergenInDish = allergens.map((allergen) => {
+                    const dishIncludesAllergen = (dish) =>
+                        dish.dishIngredients &&
+                        dish.dishIngredients.includes(allergen)
+                    return (
+                        hasDishes &&
+                        typeof meal.dishes.find(dishIncludesAllergen) ===
+                            'undefined'
+                    )
+                })
+                return isEachAllergenInDish.includes(true)
+            }
+        })
     } else {
-        return snapshot.docs.map((doc) => doc.data())
+        return rawMealsData
     }
 }
 
 export const Home = () => {
     const [meals, setMeals] = React.useState()
+    const [filters, setFilters] = React.useState([])
 
     useEffect(() => {
         getMeals().then(setMeals)
-    }, [])
+    }, [filters])
 
     return (
         <div>
-            Home
-            {JSON.stringify(meals)}
             <form
                 onSubmit={(event) => {
                     event.preventDefault()
-                    const allergens = event.target.removeIngredient.value
-                    if (allergens) {
-                        getMeals(
-                            allergens.toLowerCase().replace(' ', '').split(',')
-                        ).then(setMeals)
-                    }
+                    const newAllergen = event.target.removeIngredient.value
+                    if (newAllergen && newAllergen.length > 0)
+                        setFilters([...filters, newAllergen])
                 }}
             >
                 <TextField
@@ -65,9 +64,17 @@ export const Home = () => {
                     variant="outlined"
                 />
                 <Button variant="contained" color="primary" type="submit">
-                    Submit
+                    Add Filter
                 </Button>
             </form>
+            {filters &&
+                filters.map((filter) => (
+                    <Chip label={filter} variant="outlined"></Chip>
+                ))}
+            {meals &&
+                meals.map((meal) => (
+                    <MealListItem meal={meal} key={meal.name} />
+                ))}
         </div>
     )
 }
