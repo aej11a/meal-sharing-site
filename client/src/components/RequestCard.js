fdd5c544145d2a65cd1b7d9d703591430706a608
Andrew Jones <aej11@hotmail.com>
2021-05-01 18:03:25 -0400

    Fixed data fetching

--- client/src/components/RequestCard.js
+++ client/src/components/RequestCard.js
@@ -40,92 +40,62 @@ export function RequestCard() {
                 Functionality for get data from firestore
                 */
             try {
-                let requestArray = []
-                let userArray = []
-                const requestRes = db
-                    .collection('meal-requests')
-                    .where('InviteeId', '==', user.uid)
-                const querySnapshot = await requestRes.get()
-                if (!querySnapshot.empty) {
-                    querySnapshot.forEach((doc) => {
-                        requestArray.push(doc.data())
-                    })
-                    await Promise.all(requestArray)
-                    setRequestData(requestArray)
-                    if (requestArray.length > 0) {
-                        // map means "for each (X) in array, (do something to X) to (make corresponding element Y in the new array)"
-                        // so here it means "for each (element in requestArray), (run a query) and (put the promise in the queries array)"
-                        const queries = requestArray.map(async (doc) => {
-                            const mealRes = await db
-                                .collection('meals')
-                                .doc(doc.MealId)
-                                .get()
-                            if (mealRes.exists) {
-                                return mealRes.data()
-                            } else {
-                                console.log('No such document!')
-                            }
+                if (user) {
+                    let requestArray = []
+                    let userArray = []
+                    const requestRes = db
+                        .collection('meal-requests')
+                        .where('InviteeId', '==', user.uid)
+                    const querySnapshot = await requestRes.get()
+
+                    if (!querySnapshot.empty) {
+                        // Get all requests that match the uid
+                        querySnapshot.forEach((doc) => {
+                            requestArray.push(doc.data())
                         })
-                        await Promise.all(queries)
-                        // console.log(queries) shows whats supposed to be in mealData. setMealData does not work
-                        setMealData(queries) //Not working?
-                    }
-                    querySnapshot.forEach(async (doc) => {
-                        const userRes = await db
-                            .collection('users')
-                            .doc(doc.data().HostId)
-                            .get()
-                        if (userRes.exists) {
-                            userArray.push(userRes.data().name)
-                        } else {
-                            console.log('No such document!')
+                        setRequestData(requestArray)
+
+                        // If requests are found, get all of the meal info and user data for the request meals
+                        if (requestArray.length > 0) {
+                            // map means "for each (X) in array, (do something to X) to (make corresponding element Y in the new array)"
+                            // so here it means "for each (element in requestArray), (run a query) and (put the promise in the queries array)"
+                            const queries = requestArray.map(async (doc) => {
+                                const mealRes = await db
+                                    .collection('meals')
+                                    .doc(doc.MealId)
+                                    .get()
+                                if (mealRes.exists) {
+                                    return { ...mealRes.data(), id: doc.MealId }
+                                } else {
+                                    console.log('No such document!')
+                                }
+                            })
+                            const results = await Promise.all(queries)
+                            setMealData(results)
+
+                            querySnapshot.forEach(async (doc) => {
+                                const userRes = await db
+                                    .collection('users')
+                                    .doc(doc.data().HostId)
+                                    .get()
+                                if (userRes.exists) {
+                                    userArray.push(userRes.data().name)
+                                } else {
+                                    console.log('No such document!')
+                                }
+                            })
+                            setHostData([...userArray])
                         }
-                    })
-                    setHostData(userArray)
-                } else {
-                    console.log('No such document!')
+                    } else {
+                        console.log('No such document!')
+                    }
                 }
             } catch (error) {
                 console.log('Error getting document:', error)
             }
         }
         fetchData()
-    }, [])
-
-    /*
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                let mealArray = []
-                if(requestData){
-                    requestData.forEach(async (doc) => {
-                        const mealRes = await db
-                            .collection('meals')
-                            .doc(doc.MealId)
-                            .get()
-                        if (mealRes.exists) {
-                            mealArray.push(mealRes.data())
-                        } else {
-                            console.log('No such document!')
-                        }
-                    })
-                    setMealData(mealArray) //Not working?
-                    console.log(mealArray)
-                } else {
-                    console.log('No such document!')
-                }
-            }
-            catch (error) {
-                console.log('Error getting document:', error)
-            }
-        }
-        fetchData()
-    })
-    */
-
-    console.log(requestData)
-    console.log(mealData)
-    console.log(hostData)
+    }, [user])
 
     const getDateString = (date) => {
         return date.toLocaleDateString('en-US', {
@@ -148,46 +118,59 @@ export function RequestCard() {
             return <AccessTimeIcon />
         }
     }
-
-    const innerContent = !requestData ? (
+    console.log({
+        requestData,
+        mealData,
+        hostData,
+        condition: !(requestData && mealData && hostData),
+    })
+    const innerContent = !(requestData && mealData && hostData) ? (
         <p>Loading...</p>
     ) : (
         //This is the ui for the requests
         <>
             {requestData.map(function (request, index) {
-                ;<li key={request.MealId}>
-                    <CardHeader title={mealData[index].name} />
+                const requestMeal = mealData.filter(
+                    (meal) => meal.id === request.MealId
+                )[0]
+                console.log(
+                    mealData.filter((meal) => meal.id === request.MealId)
+                )
+                return (
+                    <li key={request.MealId}>
+                        <CardHeader title={requestMeal.name} />
 
-                    <CardContent>
-                        <div
-                            style={{
-                                display: 'grid',
-                                gridTemplateColumns: '0.1fr 1fr',
-                            }}
-                        >
-                            <AccountCircleIcon />
-                            <span>
-                                {hostData[index] && hostData[index].name}
-                            </span>
-                            <CalendarIcon />
-                            <span>
-                                {getDateString(
-                                    mealData[index] && mealData[index].time
-                                        ? new Date(mealData[index].time)
-                                        : undefined
-                                )}
-                            </span>
-                            <LocationPinIcon />
-                            <span>{mealData[index].location}</span>
-                            {/*
+                        <CardContent>
+                            <div
+                                style={{
+                                    display: 'grid',
+                                    gridTemplateColumns: '0.1fr 1fr',
+                                }}
+                            >
+                                <AccountCircleIcon />
+                                <span>
+                                    {hostData[index] && hostData[index].name}
+                                </span>
+                                <CalendarIcon />
+                                <span>
+                                    {getDateString(
+                                        mealData[index] && mealData[index].time
+                                            ? new Date(mealData[index].time)
+                                            : undefined
+                                    )}
+                                </span>
+                                <LocationPinIcon />
+                                <span>{mealData[index].location}</span>
+                                {/*
                         <ExpirationIcon />
                         <span>Expires {getDateString(expiration)}</span>
                         */}
-                            {showStatus(request.Status)}
-                            <span>{request.Status}</span>
-                        </div>
-                    </CardContent>
-                </li>
+                                {showStatus(request.Status)}
+                                <span>{request.Status}</span>
+                            </div>
+                        </CardContent>
+                    </li>
+                )
             })}
         </>
     )
