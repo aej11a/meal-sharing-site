16752bfeebc5c6c32043cbd862e44b6e49f65e56
JD Pablo <jdpablo1002@gmail.com>
2021-05-02 19:31:32 -0400

    added user location to front map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,30 +1,64 @@
 import React from 'react'
 import GoogleMapReact from 'google-map-react'
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>
+const Pin = ({ text }) => (
+    <div style={{ marginLeft: '-20px', marginTop: '-40px' }}>
+        <img style={{ height: 40 }} src="/mapPin.png" alt="Pin"></img>
+        {text}
+    </div>
+)
 
-const SimpleMap = () => {
-    return (
-        // Important! Always set the container height explicitly
-        <div style={{ height: '45vh', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{
-                    key: 'AIzaSyDTcOhMHYO4q9iqKBD75sicIxiBLJnFS2s',
-                }}
-                defaultCenter={{
-                    lat: 59.95,
-                    lng: 30.33,
-                }}
-                defaultZoom={11}
-            >
-                <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
-                    text="My Marker"
-                />
-            </GoogleMapReact>
-        </div>
-    )
+const posError = () => {
+    if (navigator.permission) {
+        navigator.permissions.query({ name: 'geolocation' }).then((res) => {
+            if (res.state === 'denied') {
+                alert('Please enable location permissions.')
+            }
+        })
+    } else {
+        alert('Unable to access location.')
+    }
+}
+
+const SimpleMap = (props) => {
+    const [center, setCenter] = React.useState()
+    const showPosition = (position) => {
+        let lat = position.coords.latitude
+        let lng = position.coords.longitude
+        setCenter({ lat, lng })
+    }
+    const getPosition = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, posError)
+        } else {
+            alert('Must be on Google Chrome for this functionality.')
+        }
+    }
+    React.useEffect(() => {
+        getPosition()
+    }, [getPosition])
+    if (center)
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '45vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{
+                        key: 'AIzaSyDTcOhMHYO4q9iqKBD75sicIxiBLJnFS2s',
+                    }}
+                    defaultCenter={center}
+                    defaultZoom={15}
+                >
+                    <Pin
+                        lat={center.lat}
+                        lng={center.lng}
+                        text="Your Location"
+                    />
+                </GoogleMapReact>
+            </div>
+        )
+    else {
+        return null
+    }
 }
 
 export default SimpleMap
