898acdcee79de21c46ed721f0086e7dfec576aed
Andrew Jones <aej11@hotmail.com>
2021-05-01 23:16:39 -0400

    Styling, and moved filters to  a separate component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,37 +1,26 @@
 import React, { useEffect } from 'react'
 import { db } from '../firebase'
 import { MealListItem } from '../components/MealListItem'
-import Button from '@material-ui/core/Button'
-import Chip from '@material-ui/core/Chip'
-import MTextField from '@material-ui/core/TextField'
-const TextField = (props) => (
-    <MTextField
-        {...props}
-        style={{ ...props.style, display: 'block', marginBottom: 10 }}
-    />
-)
+import { Filters } from '../components/Filters'
+
 const getMeals = async (allergens) => {
     const snapshot = await db.collection('meals').get()
     const rawMealsData = snapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
     }))
-    console.log(allergens)
     if (allergens && allergens.length) {
         return rawMealsData.filter((meal) => {
-            const hasDishes = !!meal.dishes && meal.dishes.length > 0
+            let isMealSafe = true
 
-            const isEachAllergenInDish = allergens.map((allergen) => {
-                const dishIncludesAllergen = (dish) =>
-                    dish.dishIngredients &&
-                    dish.dishIngredients.includes(allergen)
-                return (
-                    hasDishes &&
-                    typeof meal.dishes.find(dishIncludesAllergen) ===
-                        'undefined'
-                )
+            allergens.forEach((allergen) => {
+                meal.dishes.forEach((dish) => {
+                    if (dish.dishIngredients.includes(allergen))
+                        isMealSafe = false
+                })
             })
-            return isEachAllergenInDish.includes(true)
+
+            return isMealSafe
         })
     } else {
         return rawMealsData
@@ -48,44 +37,11 @@ export const Home = () => {
 
     return (
         <div>
-            <form
-                onSubmit={(event) => {
-                    event.preventDefault()
-                    const newAllergen = event.target.removeIngredient.value
-                    if (newAllergen && newAllergen.length > 0)
-                        setAppliedFilters([...appliedFilters, newAllergen])
-                }}
-            >
-                <TextField
-                    id="outlined-basic"
-                    label="Remove Ingredient"
-                    name="removeIngredient"
-                    variant="outlined"
-                />
-                <Button variant="contained" color="primary" type="submit">
-                    Add Filter
-                </Button>
-            </form>
-            {appliedFilters &&
-                appliedFilters.map((filter) => (
-                    <Chip
-                        label={filter}
-                        onClick={() =>
-                            setAppliedFilters(
-                                appliedFilters.filter(
-                                    (filterName) => filterName !== filter
-                                )
-                            )
-                        }
-                        onDelete={() =>
-                            setAppliedFilters(
-                                appliedFilters.filter(
-                                    (filterName) => filterName !== filter
-                                )
-                            )
-                        }
-                    />
-                ))}
+            <Filters
+                appliedFilters={appliedFilters}
+                setAppliedFilters={setAppliedFilters}
+            />
+            <br />
             {meals &&
                 meals.map((meal) => (
                     <MealListItem meal={meal} key={meal.name} />
